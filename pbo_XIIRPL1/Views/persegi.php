1d7c03a58f1ab653589f40fc0acfd27295aeb2c8
--unset-all <--unset-all>
2023-08-08 07:45:50 +0700

    first commit

---
+++ pbo_XIIRPL1/Views/persegi.php
@@ -0,0 +1,7 @@
+<?php
+require('../Class/class_perhitungan.php');
+
+$hitung= new perhitungan();
+
+ echo $hitung->luasPersegi(28);
+?>
---
+++ pbo_XIIRPL1/Views/persegi_panjang.php
@@ -0,0 +1,8 @@
+<?php
+require('../Class/class_perhitungan.php');
+
+$hitung= new perhitungan ();
+
+echo $hitung->luasPersegiPanjang(26,9);
+
+?>
